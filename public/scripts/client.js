17e150f451ea6b92ab92a5767be6f2e7d65c6793
shruthi <shruthigangadharblr@gmail.com>
2020-09-11 22:54:26 -0400

    code refactoring

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 //Handle vulnerable to XSS
 const escape = function (str) {
   let div = document.createElement('div');
@@ -36,6 +10,7 @@ const escape = function (str) {
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
+//format created at date 
 const formatDate = function (date) {
 
   var seconds = Math.floor((new Date() - date) / 1000);
@@ -107,6 +82,7 @@ const loadTweets = function () {
       renderTweets(response)
     });
 }
+//Create new tweet link to compose new tweet
 function showCreateTweet() {
   $("#create-tweet").show();
 }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
     $("#tweet-text").on('keyup change input', function () { //keypress blur input
-
         let text = $(this).val();
-
         if (text) {
             let tweetLength = 140 - text.length;
             if (tweetLength > 0) {
                 $("#characterId").removeClass("invalid").val(tweetLength);
                 $("#error-text").html("");
                 $("#error-message").hide();
-            }
-            else {
+            } else {
                 $("#characterId").addClass("invalid").val(tweetLength);
                 $("#error-text").html("Tweet character should be less than 140 characters");
                 $("#error-message").show();
             }
-        }
-        else {
-            //if user select and cut the content clear class;
+        } else {
+            //if user select and cut the content(from mouse device,cut and paste operation) clear class;
             $("#characterId").removeClass("invalid").val(140);
             $("#error-text").html("");
             $("#error-message").hide();
